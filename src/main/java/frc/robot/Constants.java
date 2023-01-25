b6ba155903985eeeb6bc03414a0b4e52237b2bc0
Nathan Bessler <NathanBessler@outlook.com>
2023-01-24 18:02:10 -0600

    added doublesolenoid controls

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -30,15 +30,15 @@ public final class Constants {
                 new Translation2d(-kWheelBase / 2, -kTrackWidth / 2),
                 new Translation2d(-kWheelBase / 2, kTrackWidth / 2));
 
-        public static final int kFrontLeftDriveMotorPort = 8;
-        public static final int kBackLeftDriveMotorPort = 2;
-        public static final int kFrontRightDriveMotorPort = 6;
-        public static final int kBackRightDriveMotorPort = 4;
+        public static final int kFrontLeftDriveMotorPort = 1;
+        public static final int kBackLeftDriveMotorPort = 5;
+        public static final int kFrontRightDriveMotorPort = 3;
+        public static final int kBackRightDriveMotorPort = 7;
 
-        public static final int kFrontLeftTurningMotorPort = 7;
-        public static final int kBackLeftTurningMotorPort = 1;
-        public static final int kFrontRightTurningMotorPort = 5;
-        public static final int kBackRightTurningMotorPort = 3;
+        public static final int kFrontLeftTurningMotorPort = 2;
+        public static final int kBackLeftTurningMotorPort = 6;
+        public static final int kFrontRightTurningMotorPort = 4;
+        public static final int kBackRightTurningMotorPort = 8;
 
         public static final boolean kFrontLeftTurningEncoderReversed = true;
         public static final boolean kBackLeftTurningEncoderReversed = true;
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,8 +5,14 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+// import edu.wpi.first.wpilibj.Compressor;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,6 +27,10 @@ public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
 
     private RobotContainer m_robotContainer;
+    private XboxController xbController = new XboxController(0);
+
+    private DoubleSolenoid doubleSolenoidOne = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+    private DoubleSolenoid doubleSolenoidTwo = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
 
     /**
      * This function is run when the robot is first started up and should be used
@@ -98,8 +108,22 @@ public class Robot extends TimedRobot {
     }
 
     /** This function is called periodically during operator control. */
+    // This is where you program buttons on the xbox controller...
     @Override
     public void teleopPeriodic() {
+        if (xbController.getLeftBumperPressed()) {
+            doubleSolenoidOne.set(kForward);
+          }
+        if (xbController.getRightBumperPressed()) {
+            doubleSolenoidOne.set(kReverse);
+          }
+        
+        if (xbController.getLeftTriggerAxis() > 50) {
+            doubleSolenoidTwo.set(kForward);
+          }
+        if (xbController.getRightTriggerAxis() > 50) {
+            doubleSolenoidTwo.set(kReverse);
+          }
     }
 
     @Override
